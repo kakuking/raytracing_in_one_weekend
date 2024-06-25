bcd5f4522e92eb7a150f81f9da3cca0b19a678b1
Dubey, Kartikeya <kdubey@ea.com>
2024-06-25 16:03:49 +0530

    Added Multisampling Anti-Aliasing

--- src/camera.h
+++ src/camera.h
@@ -8,6 +8,7 @@ class camera {
     public:
         double aspect_ratio = 1.0;
         int image_width = 100;
+        int samples_per_pixel = 10;
 
         void render(const hittable& world){
             initialize();
@@ -16,13 +17,14 @@ class camera {
 
             for (int j = 0; j < image_height; j++) {
                 std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
-                for (int i = 0; i < image_width; i++) {            
-                    auto pixel_center = pixel00_loc + (i*pixel_delta_u) + (j*pixel_delta_v);
-                    auto ray_direction = pixel_center - center;
-                    ray r(center, ray_direction);
+                for (int i = 0; i < image_width; i++) { 
+                    color pixel_color(0, 0, 0);
+                    for(int sample = 0; sample < samples_per_pixel; sample++){
+                        ray r = get_ray(i, j);
+                        pixel_color += ray_color(r, world);
+                    }           
 
-                    color pixel_color = ray_color(r, world);
-                    write_color(std::cout, pixel_color);
+                    write_color(std::cout, pixel_color * pixel_samples_scale);
                 }
             }
 
@@ -35,10 +37,13 @@ class camera {
         point3 pixel00_loc;
         vec3 pixel_delta_u;
         vec3 pixel_delta_v;
+        double pixel_samples_scale;
 
         void initialize(){
             image_height = int(image_width/aspect_ratio);
             image_height = image_height < 1 ? 1 : image_height;
+
+            pixel_samples_scale = 1.0/samples_per_pixel;
 
             // viewport dimensions
             auto viewport_height = 2.0;
@@ -68,5 +73,19 @@ class camera {
             vec3 unit_direction = uint_vector(r.direction());
             auto a = 0.5*(unit_direction.y() + 1.0);
             return color((1-a)*color(1, 1, 1) + a*color(0, 0, 1));
+        }
+
+        ray get_ray(int i, int j) const {
+            auto offset = sample_square();
+            auto pixel_sample = pixel00_loc + ((i + offset.x()) * pixel_delta_u) + ((j + offset.y()) * pixel_delta_v);
+
+            auto ray_origin = center;
+            auto ray_direction = pixel_sample - ray_origin;
+
+            return ray(ray_origin, ray_direction);
+        }
+
+        vec3 sample_square() const {
+            return vec3(random_double() - 0.5, random_double() - 0.5, 0);
         }
 };
--- src/color.h
+++ src/color.h
@@ -10,9 +10,10 @@ void write_color(std::ostream& out, const color& pixel_color) {
     auto g = pixel_color.y();
     auto b = pixel_color.z();
 
-    int rbyte = int(255.999 * r);
-    int gbyte = int(255.999 * g);
-    int bbyte = int(255.999 * b);
+    static const interval intensity(0.000, 0.999);
+    int rbyte = int(256 * intensity.clamp(r));
+    int gbyte = int(256 * intensity.clamp(g));
+    int bbyte = int(256 * intensity.clamp(b));
 
     out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
 }
