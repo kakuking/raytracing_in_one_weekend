2a0d9041a03774b811cda6a2559b2aebb7af904d
Kartikeya Dubey <kartikeyadubey2@gmail.com>
2024-06-25 18:52:10 +0530

    Added fuzziness to metals

--- src/material.h
+++ src/material.h
@@ -36,14 +36,16 @@ class lambertian: public material {
 class metal: public material {
     private:
         color albedo;
+        double fuzz;
 
     public:
-        metal(const color& albedo) : albedo(albedo) {}
+        metal(const color& albedo, double fuzz) : albedo(albedo), fuzz(fuzz < 1 ? fuzz : 1) {}
 
         bool scatter(const ray& ray_in, const hit_record& rec, color& attenuation, ray& scattered) const override {
             vec3 reflected = reflect(ray_in.direction(), rec.normal);
+            reflected = uint_vector(reflected) + fuzz * random_unit_vector();
             scattered = ray(rec.p, reflected);
             attenuation = albedo;
-            return true;
+            return (dot(scattered.direction(), rec.normal) > 0);
         }
 };
