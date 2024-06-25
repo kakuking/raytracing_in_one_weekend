a4befcccbe3c7a014c171e0b548f7f8b2daa653c
Kartikeya Dubey <kartikeyadubey2@gmail.com>
2024-06-25 23:53:20 +0530

    Added depth of field

--- src/camera.h
+++ src/camera.h
@@ -11,6 +11,14 @@ class camera {
         int samples_per_pixel = 10;
         int max_depth = 10;
 
+        double vfov = 90;
+        point3 lookfrom = point3(0, 0, 0);
+        point3 lookat = point3(0, 0, -1);
+        vec3 vup = vec3(0, 1, 0);
+
+        double defocus_angle = 0;
+        double focus_dist = 10;
+
         void render(const hittable& world){
             initialize();
 
@@ -39,6 +47,9 @@ class camera {
         vec3 pixel_delta_u;
         vec3 pixel_delta_v;
         double pixel_samples_scale;
+        vec3 u, v, w;
+        vec3 defocus_disc_u;
+        vec3 defocus_disc_v;
 
         void initialize(){
             image_height = int(image_width/aspect_ratio);
@@ -46,23 +57,34 @@ class camera {
 
             pixel_samples_scale = 1.0/samples_per_pixel;
 
-            // viewport dimensions
-            auto viewport_height = 2.0;
-            auto viewport_width = viewport_height * (double(image_width)/image_height);
 
             // Camera stuff
-            center = point3(0, 0, 0);
+            center = lookfrom;
 
-            auto focal_length = 1.0;
+            // auto focal_length = (lookfrom - lookat).length();
 
-            auto viewport_u = vec3(viewport_width, 0, 0);
-            auto viewport_v = vec3(0, -viewport_height, 0);
+            auto theta = degrees_to_radians(vfov);
+            auto h = tan(theta/2);
+            auto viewport_height = 2 * h * focus_dist;
+            auto viewport_width = viewport_height * (double(image_width)/image_height);
+
+            w = unit_vector(lookfrom - lookat);
+            u = unit_vector(cross(vup, w));
+            v = cross(w, u);
+
+            auto viewport_u = viewport_width * u;
+            auto viewport_v = viewport_height * -v;
 
             pixel_delta_u = viewport_u/image_width;
             pixel_delta_v = viewport_v/image_height;
 
-            auto viewport_upper_left = center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
+            auto viewport_upper_left = center - (focus_dist*w) - viewport_u/2 - viewport_v/2;
             pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+            auto defocus_radius = focus_dist * tan(degrees_to_radians(defocus_angle/2));
+
+            defocus_disc_u = u * defocus_radius;
+            defocus_disc_v = v * defocus_radius;
         }
 
         color ray_color(const ray& r, int depth, const hittable& world) const {
@@ -88,10 +110,16 @@ class camera {
             auto offset = sample_square();
             auto pixel_sample = pixel00_loc + ((i + offset.x()) * pixel_delta_u) + ((j + offset.y()) * pixel_delta_v);
 
-            auto ray_origin = center;
+            auto ray_origin = (defocus_angle <= 0) ? center: defocus_disc_sample();
             auto ray_direction = pixel_sample - ray_origin;
 
             return ray(ray_origin, ray_direction);
+        }
+
+        point3 defocus_disc_sample() const {
+            auto p = random_in_unit_disk();
+
+            return center + (p[0] * defocus_disc_u) + (p[1] * defocus_disc_v);
         }
 
         vec3 sample_square() const {
--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,14 @@ int main(){
 
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
     auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-    auto material_left = make_shared<dielectric>(1.0/1.33);
+    auto material_left = make_shared<dielectric>(1.5);
+    auto material_bubble = make_shared<dielectric>(1.0/1.5);
     auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
 
     world.add(make_shared<sphere>(point3(0, -100.5, -1), 100, material_ground));
     world.add(make_shared<sphere>(point3(0.0, 0.0, -1.2), 0.5, material_center));
     world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.2), 0.5, material_left));
+    world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.2), 0.4, material_bubble));
     world.add(make_shared<sphere>(point3(1.0, 0.0, -1.2), 0.5, material_right));
 
 
@@ -24,6 +26,14 @@ int main(){
     cam.image_width = 400;
     cam.samples_per_pixel = 100;
     cam.max_depth = 20;
+
+    cam.vfov = 20;
+    cam.lookfrom = point3(-2, 2, 1);
+    cam.lookat = point3(0, 0, -1);
+    cam.vup = vec3(0, 1, 0);
+
+    cam.defocus_angle = 10.0;
+    cam.focus_dist = 3.4;
 
     cam.render(world);
 }
